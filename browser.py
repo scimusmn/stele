66dca5eaba2076634df26667c001a80811690171
bryan kennedy <bkennedy@smm.org>
2013-04-04 21:14:31 -0500

    Importing the initial modules that we will need

---
+++ browser.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+""" Browser control system """
+
+from init import get_machines_config
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
