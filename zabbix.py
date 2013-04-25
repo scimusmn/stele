b8106a56e0a7a4ad86c80a3f5d650ca96244ba4c
bryan kennedy <bkennedy@smm.org>
2013-04-24 21:54:33 -0500

    Cleaning up the Zabbix system
    Proper config values in the default config
    Better logic for the sended command

--- zabbix.py
+++ zabbix.py
@@ -6,29 +6,27 @@ status updates about the kiosk operations for central monitoring.
 """
 
 import init
-import time
+from init import get_machines_config
+
+CFG = get_machines_config()
 
 
-def send_zabbix_message(command_type):
+def send_zabbix_message(key, value):
     """Send Zabbix a message
 
     Args:
-        type: What type of show action occurred
+        key: Zabbix key to be tracked
+        value: The value to send to Zabbix
 
     Returns:
         A boolean indicating if the commands were sent with (1) or
         without (0) any errors.
     """
-
-    # Push a notification to Zabbix if the PEDT show is started
-    timestamp = str(time.time())
-    # TODO check for the presence of this script
-
-    # -s: The name of this 'host' in the Zabbix system
-    # -k: The key or message to send to Zabbix
-    # TODO: Get the -s value from a config file
-    command = 'zabbix_sender -c /etc/zabbix/zabbix_agentd.conf\
-            -s example -k %s -o %d' % (command_type, timestamp)
+    command = 'zabbix_sender -c %s -s %s -k %s -o %d' % (
+        CFG['browser']['zabbix_agentd_cfg'],
+        CFG['browser']['zabbix_hostname'],
+        key,
+        value)
 
     print
     output, error = init.execute_shell_command(command)
