90752c34848b81a1f6d0ef57c95f7166c7cd5181
bryan kennedy <bkennedy@smm.org>
2013-04-24 22:14:50 -0500

    Better exception handlng & msgs for cfg copying

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,20 @@
 """A setup file for the Stele kiosk system"""
 
 from fabric.api import local
+import os
 import shutil
 
 
 def install():
     """Install the Stele kiosk browser system"""
-    try:
-        shutil.copy('cfg/browser.cfg.default', 'cfg/browser.cfg')
-    except:
-        print 'File was not copied.'
+    cfg_src = 'cfg/browser.cfg.default'
+    cfg_dst = 'cfg/browser.cfg'
+    if not os.exists(cfg_src, cfg_dst):
+        try:
+            shutil.copy(cfg_src, cfg_dst)
+            print 'The config file is ready for modification.'
+        except:
+            print 'There was a problem and the config file wasn\'t setup.'
+    else:
+        print 'There is already a config file in place. Please edit it.'
+
