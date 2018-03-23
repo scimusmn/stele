bd37099a527af5833421393098c82b8de43878a9
bryan kennedy <bkennedy@smm.org>
2018-03-23 10:01:11 -0500

    Comment style cleanup

--- app/background.js
+++ app/background.js
@@ -1,10 +1,10 @@
-/**
- * Background
- *
- * This is main Electron process, started first thing when your app launches.
- * This script runs through entire life of your application. It doesn't have
- * any windows that you can see on screen, but we can open windows from here.
- */
+//
+// Background
+//
+// This is main Electron process, started first thing when your app launches.
+// This script runs through entire life of your application. It doesn't have
+// any windows that you can see on screen, but we can open windows from here.
+//
 
 import jetpack from 'fs-jetpack';
 
@@ -33,31 +33,31 @@ app.on('ready', function () {
     height: 600,
   });
 
-  /**
-   * Hack to make full-screen kiosk mode actually work.
-   *
-   * There is an active bug with Electron, kiosk mode, and Yosemite.
-   * https://github.com/atom/electron/issues/1054
-   * This hack makes kiosk mode actually work by waiting for the app to launch
-   * and then issuing a call to go into kiosk mode after a few milliseconds.
-   */
+  //
+  // Hack to make full-screen kiosk mode actually work.
+  //
+  // There is an active bug with Electron, kiosk mode, and Yosemite.
+  // https://github.com/atom/electron/issues/1054
+  // This hack makes kiosk mode actually work by waiting for the app to launch
+  // and then issuing a call to go into kiosk mode after a few milliseconds.
+  //
   if (env.name == 'production') {
     setTimeout(function () {
       mainWindow.setKiosk(true);
     }, 100);
   }
 
-  /**
-   * Show dev tools when we're not in production mode
-   */
+  //
+  // Show dev tools when we're not in production mode
+  //
   if (env.name !== 'production') {
     devHelper.setDevMenu();
     mainWindow.openDevTools();
   }
 
-  /**
-   * Open the app
-   */
+  //
+  // Open the app
+  //
   console.log(`This platform is ${process.platform}`);
   if (env.name === 'test') {
     mainWindow.loadURL('file://' + __dirname + '/spec.html');
@@ -81,15 +81,15 @@ app.on('ready', function () {
     loadWindowConfigFile(configFile);
   }
 
-  /**
-   * Keyboard shortcuts
-   *
-   * Ctrl or Command + f will switch you to the Finder.
-   * We use the "switch to Finder" approach instead of a quit, because in most
-   * of our Electron setups we have a launchd process that will relaunch the
-   * app on quit. For maintenance, we probably just need to be able to get
-   * to the Finder while the application remains running in the background.
-   */
+  //
+  // Keyboard shortcuts
+  //
+  // Ctrl or Command + f will switch you to the Finder.
+  // We use the "switch to Finder" approach instead of a quit, because in most
+  // of our Electron setups we have a launchd process that will relaunch the
+  // app on quit. For maintenance, we probably just need to be able to get
+  // to the Finder while the application remains running in the background.
+  //
   const retQuit = globalShortcut.register('CommandOrControl+F', () => {
     console.log('Switching to Finder');
     promisedExec('open -a Finder');
@@ -97,6 +97,7 @@ app.on('ready', function () {
   if (!retQuit) {
     console.log('Quit keyboard registration failed');
   }
+
   const retReload = globalShortcut.register('CommandOrControl+R', () => {
     console.log('Reload the page');
     mainWindow.reload();
