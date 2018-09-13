21ab1a53daa1af22a3d8779a9a829c7782e3da7f
bkennedy <bkennedy@smm.org>
2018-09-13 15:09:16 -0500

    Add `open finder` keyboard shortcut
    Adding this back from the pre-rebase application. Without this you can
    get locked out of the computer when the system goes into kiosk mode.

--- src/background.js
+++ src/background.js
@@ -5,12 +5,14 @@
 
 import path from "path";
 import url from "url";
-import { app, Menu } from "electron";
+import { app, Menu, globalShortcut } from "electron";
 import jetpack from "fs-jetpack";
 import os from 'os';
 import { devMenuTemplate } from "./menu/dev_menu_template";
 import { editMenuTemplate } from "./menu/edit_menu_template";
 import createWindow from "./helpers/window";
+let childProcess = require('child_process');
+let promisedExec = childProcess.exec;
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
@@ -98,6 +100,24 @@ app.on("ready", () => {
   if (env.name === "development") {
     mainWindow.openDevTools();
   }
+
+
+  //
+  // Keyboard shortcuts
+  //
+  // Ctrl or Command + f will switch you to the Finder.
+  // We use the "switch to Finder" approach instead of a quit, because in most
+  // of our Electron setups we have a launchd process that will relaunch the
+  // app on quit. For maintenance, we probably just need to be able to get
+  // to the Finder while the application remains running in the background.
+  //
+  // TODO: Make this work on Windows and Linux
+  //
+  const retQuit = globalShortcut.register('CommandOrControl+F', () => {
+    console.log('Switching to Finder');
+    promisedExec('open -a Finder');
+  });
+
 });
 
 app.on("window-all-closed", () => {
