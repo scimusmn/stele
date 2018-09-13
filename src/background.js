d09f8014e9838776ae80d517b76cd27bdda60edb
bkennedy <bkennedy@smm.org>
2018-09-13 13:47:08 -0500

    Add config file and delay back into rebase
    This adds existing functionality of the original wrapper back into the
    new boilerplate code.
    
    Work on #5

--- src/background.js
+++ src/background.js
@@ -6,6 +6,8 @@
 import path from "path";
 import url from "url";
 import { app, Menu } from "electron";
+import jetpack from "fs-jetpack";
+import os from 'os';
 import { devMenuTemplate } from "./menu/dev_menu_template";
 import { editMenuTemplate } from "./menu/edit_menu_template";
 import createWindow from "./helpers/window";
@@ -22,6 +24,47 @@ const setApplicationMenu = () => {
   Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
 };
 
+//
+// Delay app loading until the system has been up for a few seconds
+//
+function loadWindowUptimeDelay(window, configFileObj) {
+  // Seconds since launch, when it will be safe to load the URL
+  const nominalUptime = 300;
+
+  // Seconds to wait if we are not in the nominal uptime window
+  const launchDelay = 60;
+
+  if (os.uptime() > nominalUptime) {
+    console.log('Launching immediately');
+    window.loadURL(configFileObj.url);
+  } else {
+    console.log('Delaying launch ' + launchDelay + ' seconds');
+    window.loadURL('file://' + __dirname + '/launch-delay.html?delay=' + launchDelay);
+    setTimeout(function() {
+      window.loadURL(configFileObj.url);
+    }, launchDelay * 1000);
+  }
+}
+
+//
+// Read config file and pass app url to the window opener
+//
+function loadWindowConfigFile(window, configFile) {
+  const configFileObj = jetpack.read(configFile, 'json');
+  if (configFileObj !== null) {
+    loadWindowUptimeDelay(window, configFileObj);
+  } else {
+    console.log('Config file [' + configFile + '] not present.');
+    window.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "config-error.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+  }
+}
+
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
 // on same machine like those are two separate apps.
@@ -34,17 +77,23 @@ app.on("ready", () => {
   setApplicationMenu();
 
   const mainWindow = createWindow("main", {
-    width: 1000,
+    width: 400,
     height: 600
   });
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "app.html"),
-      protocol: "file:",
-      slashes: true
-    })
-  );
+  //
+  // Kiosk mode
+  //
+  // Enable fullscreen kiosk mode in production
+  //
+  if (env.name === 'production') {
+    mainWindow.setKiosk(true);
+  }
+
+  //
+  // Open window with configured URL
+  //
+  loadWindowConfigFile(mainWindow, '/usr/local/etc/kiosk/config.json');
 
   if (env.name === "development") {
     mainWindow.openDevTools();
