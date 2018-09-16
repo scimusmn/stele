43d797cc255dee8994cfba9eb38f79a5435577a8
bkennedy <bkennedy@smm.org>
2018-09-16 17:24:21 -0500

    style: Cleanup quotes and othe ES6 items
    Mostly focusing on webpack config.

--- build/webpack.app.config.js
+++ build/webpack.app.config.js
@@ -1,15 +1,13 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const base = require("./webpack.base.config");
+const path = require('path');
+const merge = require('webpack-merge');
+const base = require('./webpack.base.config');
 
-module.exports = env => {
-  return merge(base(env), {
-    entry: {
-      background: "./src/background.js",
-    },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "../app")
-    }
-  });
-};
+module.exports = env => merge(base(env), {
+  entry: {
+    background: './src/background.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../app'),
+  },
+});
--- src/app.js
+++ src/app.js
@@ -1,4 +1,1 @@
 import './stylesheets/main.css';
-
-// Small helpers you might want to keep
-import './helpers/external_links';
