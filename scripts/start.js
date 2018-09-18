2be8e51e8ac58c98ac0d3b609ef12b4f6b69895d
bkennedy <bkennedy@smm.org>
2018-09-18 10:25:56 -0500

    feat: Move webpack config to scripts
    Push the old electron config out of the way and point the start script
    to the new react webpack config.
    
    This still doesn't properly launch our Electron app.
    
    Issues: #3

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,9 @@
 const childProcess = require('child_process');
 const electron = require('electron');
 const webpack = require('webpack');
-const config = require('./webpack.app.config');
+const config = require('./webpack.config');
 
-const env = 'development';
-const compiler = webpack(config(env));
+const compiler = webpack(config);
 let electronStarted = false;
 
 const watching = compiler.watch({}, (err, stats) => {
