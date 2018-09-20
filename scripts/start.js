bae72414ea17bb716247a4c2804a95ec2bbf3e79
bkennedy <bkennedy@smm.org>
2018-09-20 09:12:24 -0500

    refactor: Improve webpack env var handling
    Rename files and pass the environment variable to eliminate the need for
    two webpack configs for the production and dev environments.

--- scripts/start.js
+++ scripts/start.js
@@ -1,9 +1,15 @@
+//
+// Dev launcher script
+//
+// Run webpack dev config, and then launch Electron
+//
 const childProcess = require('child_process');
 const electron = require('electron');
 const webpack = require('webpack');
-const config = require('../config/webpack.config');
+const config = require('../config/webpack.config.base');
 
-const compiler = webpack(config);
+const env = 'development';
+const compiler = webpack(config(env));
 let electronStarted = false;
 
 const watching = compiler.watch({}, (err, stats) => {
