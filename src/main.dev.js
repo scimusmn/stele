3bee1681d429ad26cb95b7310749aaa87b2db33c
bkennedy <bkennedy@smm.org>
2018-12-04 16:39:54 -0600

    feat: Implement basic dev menus for app
    We only want a menu in dev mode. In production, this app is intended for
    kiosks and shouldn't show a menu bar.

--- src/main.dev.js
+++ src/main.dev.js
@@ -8,8 +8,12 @@
 // When running `yarn build` or `yarn build-main`, this file is compiled to
 // `./app/main.prod.js` using webpack. This gives us some performance wins.
 //
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, globalShortcut, ipcMain } from 'electron';
 import settings from 'electron-settings';
+import childProcess from 'child_process';
+import MenuBuilder from './menu';
+
+const promisedExec = childProcess.exec;
 
 let mainWindow = null;
 
@@ -88,10 +92,27 @@ app.on('ready', async () => {
     console.log('----^ ^ ^ ^ ^ arg ^ ^ ^ ^ ^----');
   });
 
+  //
+  // Keyboard shortcuts
+  //
+  // Ctrl or Command + f will switch you to the Finder.
+  // We use the "switch to Finder" approach instead of a quit, because in most
+  // of our Electron setups we have a launchd process that will relaunch the
+  // app on quit. For maintenance, we probably just need to be able to get
+  // to the Finder while the application remains running in the background.
+  //
+  // TODO: Make this work on Windows and Linux
+  //
+  globalShortcut.register('Control+F', () => {
+    console.log('Switching to Finder');
+    promisedExec('open -a Finder');
+  });
+
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 
-  // const menuBuilder = new MenuBuilder(mainWindow);
-  // menuBuilder.buildMenu();
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
 });
---
+++ src/menu.js
@@ -0,0 +1,251 @@
+import { app, Menu, shell } from 'electron';
+
+export default class MenuBuilder {
+  constructor(mainWindow) {
+    this.mainWindow = mainWindow;
+  }
+
+  buildMenu() {
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.DEBUG_PROD === 'true'
+    ) {
+      this.setupDevelopmentEnvironment();
+
+      const template =
+        process.platform === 'darwin'
+          ? this.buildDarwinTemplate()
+          : this.buildDefaultTemplate();
+
+      const menu = Menu.buildFromTemplate(template);
+      Menu.setApplicationMenu(menu);
+      return menu;
+
+    }
+
+    // Only show a menu on dev for our kiosk app
+    return null;
+  }
+
+  setupDevelopmentEnvironment() {
+    this.mainWindow.openDevTools();
+    this.mainWindow.webContents.on('context-menu', (e, props) => {
+      const { x, y } = props;
+
+      Menu.buildFromTemplate([
+        {
+          label: 'Inspect element',
+          click: () => {
+            this.mainWindow.inspectElement(x, y);
+          }
+        }
+      ])
+        .popup(this.mainWindow);
+    });
+  }
+
+  buildDarwinTemplate() {
+    const subMenuAbout = {
+      label: 'Stele',
+      submenu: [
+        {
+          label: 'About ElectronReact',
+          selector: 'orderFrontStandardAboutPanel:'
+        },
+        { type: 'separator' },
+        {
+          label: 'Services',
+          submenu: []
+        },
+        { type: 'separator' },
+        {
+          label: 'Hide ElectronReact',
+          accelerator: 'Command+H',
+          selector: 'hide:'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          selector: 'hideOtherApplications:'
+        },
+        {
+          label: 'Show All',
+          selector: 'unhideAllApplications:'
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    };
+    const subMenuViewDev = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: () => {
+            this.mainWindow.webContents.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: 'Alt+Command+I',
+          click: () => {
+            this.mainWindow.toggleDevTools();
+          }
+        }
+      ]
+    };
+    const subMenuViewProd = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Toggle Full Screen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          }
+        }
+      ]
+    };
+    const subMenuWindow = {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:'
+        },
+        {
+          label: 'Close',
+          accelerator: 'Command+W',
+          selector: 'performClose:'
+        },
+        { type: 'separator' },
+        {
+          label: 'Bring All to Front',
+          selector: 'arrangeInFront:'
+        }
+      ]
+    };
+    const subMenuHelp = {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Documentation',
+          click() {
+            shell.openExternal(
+              'https://github.com/scimusmn/stele#readme'
+            );
+          }
+        },
+        {
+          label: 'Search Issues',
+          click() {
+            shell.openExternal('https://github.com/scimusmn/stele/issues');
+          }
+        }
+      ]
+    };
+
+    const subMenuView =
+      process.env.NODE_ENV === 'development' ? subMenuViewDev : subMenuViewProd;
+
+    return [subMenuAbout, subMenuView, subMenuWindow, subMenuHelp];
+  }
+
+  buildDefaultTemplate() {
+    const templateDefault = [
+      {
+        label: '&File',
+        submenu: [
+          {
+            label: '&Open',
+            accelerator: 'Ctrl+O'
+          },
+          {
+            label: '&Close',
+            accelerator: 'Ctrl+W',
+            click: () => {
+              this.mainWindow.close();
+            }
+          }
+        ]
+      },
+      {
+        label: '&View',
+        submenu:
+          process.env.NODE_ENV === 'development'
+            ? [
+              {
+                label: '&Reload',
+                accelerator: 'Ctrl+R',
+                click: () => {
+                  this.mainWindow.webContents.reload();
+                }
+              },
+              {
+                label: 'Toggle &Full Screen',
+                accelerator: 'F11',
+                click: () => {
+                  this.mainWindow.setFullScreen(
+                    !this.mainWindow.isFullScreen()
+                  );
+                }
+              },
+              {
+                label: 'Toggle &Developer Tools',
+                accelerator: 'Alt+Ctrl+I',
+                click: () => {
+                  this.mainWindow.toggleDevTools();
+                }
+              }
+            ]
+            : [
+              {
+                label: 'Toggle &Full Screen',
+                accelerator: 'F11',
+                click: () => {
+                  this.mainWindow.setFullScreen(
+                    !this.mainWindow.isFullScreen()
+                  );
+                }
+              }
+            ]
+      },
+      {
+        label: '&Help',
+        submenu: [
+          {
+            label: 'Documentation',
+            click() {
+              shell.openExternal(
+                'https://github.com/scimusmn/stele#readme'
+              );
+            }
+          },
+          {
+            label: 'Search Issues',
+            click() {
+              shell.openExternal('https://github.com/scimusmn/stele/issues');
+            }
+          }
+        ]
+      }
+    ];
+
+    return templateDefault;
+  }
+}
