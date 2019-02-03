d70f8a08a98fa95af700beeb20ad055bc8a6aebb
bkennedy <bkennedy@smm.org>
2019-02-03 15:55:51 -0600

    docs: Improve comment documentation
    - Standardize comment block style
    - Describe the purpose of some of the configuration

--- config/webpack.config.renderer.dev.babel.js
+++ config/webpack.config.renderer.dev.babel.js
@@ -1,11 +1,8 @@
 /* eslint global-require: off, import/no-dynamic-require: off */
 
-/**
- * Build config for development electron renderer process that uses
- * Hot-Module-Replacement
- *
- * https://webpack.js.org/concepts/hot-module-replacement/
- */
+//
+// Development config, using Hot Module Replacement
+//
 
 import path from 'path';
 import fs from 'fs';
@@ -26,9 +23,9 @@ const requiredByDLLConfig = module.parent.filename.includes(
   'webpack.config.renderer.dev.dll'
 );
 
-/**
- * Warn if the DLL is not built
- */
+//
+// Warn if the DLL is not built
+//
 if (!requiredByDLLConfig && !(fs.existsSync(dll) && fs.existsSync(manifest))) {
   console.log(
     chalk.black.bgYellow.bold(
@@ -210,18 +207,16 @@ export default merge.smart(baseConfig, {
 
     new webpack.NoEmitOnErrorsPlugin(),
 
-    /**
-     * Create global constants which can be configured at compile time.
-     *
-     * Useful for allowing different behaviour between development builds and
-     * release builds
-     *
-     * NODE_ENV should be production so that modules do not perform certain
-     * development checks
-     *
-     * By default, use 'development' as NODE_ENV. This can be overriden with
-     * 'staging', for example, by changing the ENV variables in the npm scripts
-     */
+    //
+    // Create global constants which can be configured at compile time.
+    //
+    // Useful for allowing different behaviour between development builds and release builds
+    //
+    // NODE_ENV should be production so that modules do not perform certain development checks
+    //
+    // By default, use 'development' as NODE_ENV. This can be overriden with 'staging', for example,
+    // by changing the ENV variables in the npm scripts
+    //
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development'
     }),
@@ -239,13 +234,17 @@ export default merge.smart(baseConfig, {
   devServer: {
     port,
     publicPath,
+    // Enable gzip compression of generated files.
     compress: true,
+    // Help with DLL features
     noInfo: true,
     stats: 'errors-only',
     inline: true,
     lazy: false,
+    // Enabel Hot Module Reload
     hot: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
+    // TODO: update this when we refactor React app folder
     contentBase: path.join(__dirname, 'dist'),
     watchOptions: {
       aggregateTimeout: 300,
