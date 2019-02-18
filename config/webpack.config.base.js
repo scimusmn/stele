0258b4dc5ccd289bc44411e59901cccdf9444bc4
bkennedy <bkennedy@smm.org>
2019-02-18 11:56:58 -0600

    feat: Enable a oneOf base config for modules
    This will setup a default file loader that catches all undefined files
    (fonts, images, etc). Move the JSX loader into the conditional defined
    in CRA.
    
    Issues: #64

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -11,14 +11,53 @@ export default {
       // Disable require.ensure as it's not a standard language feature.
       { parser: { requireEnsure: false } },
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
+        // "oneOf" will traverse all following loaders until one will
+        // match the requirements. When no loader matches it will fall
+        // back to the "file" loader at the end of the loader list.
+        oneOf: [
+          // "url" loader works like "file" loader except that it embeds assets
+          // smaller than specified limit in bytes as data URLs to avoid requests.
+          // A missing `test` is equivalent to a match.
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve('url-loader'),
+            options: {
+              // TODO: Test this path by adding an image larger than 10k
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
           },
-        },
+
+          // Primary JS adn JSX module
+          // TODO: Review CRA rules and add any useful features.
+          {
+            test: /\.(js|mjs|jsx)?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          },
+
+          // "file" loader makes sure those assets get served by WebpackDevServer.
+          // When you `import` an asset, you get its (virtual) filename.
+          // In production, they would get copied to the `build` folder.
+          // This loader doesn't use a "test" so it will catch all modules
+          // that fall through the other loaders.
+          {
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise be processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|mjs|jsx)$/, /\.html$/, /\.json$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
