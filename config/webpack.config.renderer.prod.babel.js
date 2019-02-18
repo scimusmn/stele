20e1b036129ebc77d593c61bcc4ade175b218918
bkennedy <bkennedy@smm.org>
2019-02-18 11:59:07 -0600

    feat: Remove font mods. covered by file catchall
    The file catch all module handles these us the url-loader in the base
    config, so we don't need to define individual file types.
    
    Issues: #64

--- config/webpack.config.renderer.prod.babel.js
+++ config/webpack.config.renderer.prod.babel.js
@@ -109,44 +109,6 @@ export default merge.smart(baseConfig, {
           },
         ],
       },
-      // WOFF Font
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/font-woff',
-          },
-        },
-      },
-      // WOFF2 Font
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/font-woff',
-          },
-        },
-      },
-      // TTF Font
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/octet-stream',
-          },
-        },
-      },
-      // EOT Font
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: 'file-loader',
-      },
       // SVG Font
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
