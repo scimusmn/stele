7c457362fbc6382831bc9adea42042ac7c29d1f9
bkennedy <bkennedy@smm.org>
2019-05-29 12:08:14 -0500

    style: Simplify cookie form component
    This can be a stateless component. It doesn't need an empty contructor.

--- src/renderer/Settings/cookieForm.js
+++ src/renderer/Settings/cookieForm.js
@@ -1,106 +1,90 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  FormText,
-  Input,
-  Col,
-  Row,
-  Table,
+  Col, FormText, Input, Row, Table,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Field } from 'formik';
 
-class CookieForm extends Component {
-  constructor(props) {
-    super(props);
+const CookieForm = (props) => {
+  const { values, handleChange, handleBlur } = props;
+  return (
+    <Row className="mt-3 justify-content-center">
+      <Col>
+        <hr />
+        <h2>Auth cookie</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+              <th>URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Field
+                  name="cookieName"
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      className="form-control"
+                      type="text"
+                      value={values.cookieName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  )}
+                />
+                <FormText>
+                  (&rsquo;access-token&rsquo;)
+                </FormText>
+              </td>
+              <td>
+                <Field
+                  name="cookieValue"
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      className="form-control"
+                      type="text"
+                      value={values.cookieValue}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  )}
+                />
+                <FormText>
+                  (&rsquo;my-token-ABC123&rsquo;)
+                </FormText>
+              </td>
+              <td>
+                <Field
+                  name="cookieURL"
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      type="text"
+                      value={values.cookieURL}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  )}
+                />
+                <FormText>
+                  Base URL of associated site.
+                  {' '}
+                  (e.g, &rsquo;http://localhost&rsquo;, &rsquo;http://smm.org&rsquo;)
+                </FormText>
+              </td>
+            </tr>
 
-    this.state = {
-
-    };
-  }
-
-  componentWillMount() {
-
-  }
-
-  render() {
-    const { values, handleChange, handleBlur } = this.props;
-    return (
-      <Row className="mt-3 justify-content-center">
-        <Col>
-          <hr />
-          <h2>Auth cookie</h2>
-          <Table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Value</th>
-                <th>URL</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <Field
-                    name="cookieName"
-                    render={({ field }) => (
-                      <Input
-                        {...field}
-                        className="form-control"
-                        type="text"
-                        value={values.cookieName}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    )}
-                  />
-                  <FormText>
-                     (&rsquo;access-token&rsquo;)
-                  </FormText>
-                </td>
-                <td>
-                  <Field
-                    name="cookieValue"
-                    render={({ field }) => (
-                      <Input
-                        {...field}
-                        className="form-control"
-                        type="text"
-                        value={values.cookieValue}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    )}
-                  />
-                  <FormText>
-                     (&rsquo;my-token-ABC123&rsquo;)
-                  </FormText>
-                </td>
-                <td>
-                  <Field
-                    name="cookieURL"
-                    render={({ field }) => (
-                      <Input
-                        {...field}
-                        type="text"
-                        value={values.cookieURL}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    )}
-                  />
-                  <FormText>
-                     Base URL of associated site. (e.g, &rsquo;http://localhost&rsquo;, &rsquo;http://smm.org&rsquo;)
-                  </FormText>
-                </td>
-              </tr>
-
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    );
-  }
-}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
 
 CookieForm.propTypes = {
   values: PropTypes.instanceOf(Object).isRequired,
