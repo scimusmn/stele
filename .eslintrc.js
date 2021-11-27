a0360795174272dccb66b215004dd5c3bda7d0e5
bkennedy <bkennedy@smm.org>
2021-11-27 14:32:12 -0600

    feat: Update eslint to js file
    By default we want ESLint files to be loaded as .js files. This matches
    our organizational style.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+//
+// Set Javascript style rules defined by ESLint
+//
+// Configure ESLint to help keep our code in alignments with our style guidelines:
+// https://smm.atlassian.net/wiki/spaces/MED/pages/6488251/Code+Style+Guide
+//
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    'function-paren-newline': ['error', 'consistent'],
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+        ],
+      },
+    ],
+    'promise/param-names': 'error',
+    'promise/always-return': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'off',
+  },
+  plugins: ['import', 'promise', 'compat', 'react'],
+  settings: {
+    polyfills: ['promises'],
+    'import/resolver': {
+      webpack: {
+        config: 'config/webpack.config.eslint.js',
+      },
+    },
+  },
+};
