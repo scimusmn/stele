684c7c847c20907e3cd19156050f7f5048358680
bkennedy <bkennedy@smm.org>
2021-12-01 21:42:41 -0600

    fix: Alphabetize rule

--- config/webpack.config.renderer.dev.babel.js
+++ config/webpack.config.renderer.dev.babel.js
@@ -43,11 +43,11 @@ if (!requiredByDLLConfig && !(fs.existsSync(dll) && fs.existsSync(manifest))) {
 }
 
 export default merge(baseConfig, {
-  mode: 'development',
-
   // Good balance of rebuild speed and dev helpfulness. Copying our approach from create-react-app
   // See the discussion in https://github.com/facebook/create-react-app/issues/343
   devtool: 'cheap-module-source-map',
+
+  mode: 'development',
 
   output: {
     path: webpackPaths.distRendererPath,
